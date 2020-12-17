2cf0255d73bb7360cbcde99f73d48a9d1d4a02a7
bikaylee <kaylee1042@gmail.com>
2020-12-17 11:15:04 -0800

    Update plot.py

--- src/plot.py
+++ src/plot.py
@@ -1,19 +1,17 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-
-def plotGraph(fileName, xLabel, yLabel):
-
+def plotGraph(fileName, xLabel, yLabel, category):
 	log_frequency = 50
 	box = np.ones(log_frequency) /log_frequency
-	data = np.genfromtxt(fileName+'.txt', delimiter="\t")
+	data = np.genfromtxt(fileName+'.txt', delimiter=" ")
 
 	X = []
 	Y = []
 
 	for item in data:
 		X.append(item[0])
-		Y.append(item[1])
+		Y.append(item[category])
 		if len(Y) > log_frequency and len(Y) % log_frequency == 0:
 			Y_smooth = np.convolve(Y, box, mode='same')
 			plt.clf()
@@ -21,11 +19,10 @@ def plotGraph(fileName, xLabel, yLabel):
 			plt.title('Pixel Jump')
 			plt.ylabel(yLabel)
 			plt.xlabel(xLabel)
-			plt.savefig(fileName+'.png')
+			plt.savefig(xLabel + ' vs ' + yLabel +'.png')
 
-
-plotGraph("returns", "Steps", "Return")
-plotGraph("differences_returns","Steps", "Differences")
-plotGraph("distance_returns", "Episodes", "Distances")
-
+plotGraph("steps", "Steps", "Scores", 1)
+plotGraph("steps","Steps", "Relative Differences", 2)
+plotGraph("episodes", "Episodes", "Steps", 1)
+plotGraph("episodes", "Episodes", "Scores", 2)
 
